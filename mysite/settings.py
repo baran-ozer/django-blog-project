834339a1b965482d5cad2df7f7488cc6196fd014
baran-ozer <baran-ozer@outlook.com>
2021-07-06 15:34:09 +0300

    host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fbbk(5ay@2qc^^+3^lpj3r1x)su+6d(pqn42%7=xehyrgo0m@f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.198.79.142 ']
+ALLOWED_HOSTS = ['18.198.79.142']
 
 
 # Application definition

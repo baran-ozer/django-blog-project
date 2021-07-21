6eaa0aed3b56f00a898f576b81b54402c97524f4
baran-ozer <baran-ozer@outlook.com>
2021-07-21 16:44:49 +0300

    wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/opt/bitnami/projects/shipping-prices')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/projects/shipping-prices/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
 application = get_wsgi_application()
+
